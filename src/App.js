c9d0ce38e770099c76c933dff052d1380fd23d28
David Kim <hola.hoon@gmail.com>
2020-08-30 17:17:04 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+    <div className='main'>
+      <h2>
+        You are being redirected to{" "}
         <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://www.erugogada.com/'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Learn React
+          erugogada.com
         </a>
-      </header>
+      </h2>
     </div>
   );
 }
